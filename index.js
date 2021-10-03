c075f40c90fef53c9a2afc121d343fa6dd010d97
VIVEK GUPTA <vivek.jmdsmvdu@gmail.com>
2021-10-03 21:39:05 +0530

    added

--- index.js
+++ index.js
@@ -1,6 +1,22 @@
+/*
 var a = 2;
 var b = "2";
 var c = true;
+var d = "hallo";
 console.log(typeof a);
 console.log(typeof b);
 console.log(typeof c);
+console.log(typeof d);
+console.log("hallo world");
+*/
+
+var x = undefined;
+var y = null;
+console.log(typeof y);
+
+var c = 6;
+
+// Diff between == & ===
+
+// 5 == 5
+console.log(5 === 5)
